28b6a06e4c64589f48ed2b0ce71ad8d1c3162344
Oleg Pruh <olegpruh1@gmail.com>
2023-11-23 17:45:17 +0000

    third commit

--- src/main/java/com/olehprukhnytskyi/chat/util/ChatGPTUtil.java
+++ src/main/java/com/olehprukhnytskyi/chat/util/ChatGPTUtil.java
@@ -1,5 +1,6 @@
 package com.olehprukhnytskyi.chat.util;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.io.IOException;
@@ -38,8 +39,15 @@ public class ChatGPTUtil {
     }
 
     public String extractMessageFromJSONResponse(String response) {
-        int start = response.indexOf("content") + 11;
-        int end = response.indexOf("\"", start);
-        return response.substring(start, end);
+        try {
+            return new ObjectMapper().readTree(response)
+                    .path("choices")
+                    .path(0)
+                    .path("message")
+                    .path("content")
+                    .asText();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't extract message", e);
+        }
     }
 }
